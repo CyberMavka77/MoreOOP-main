706dc7a3ee3fc4f5cbeeddbb5e3c2ef14648c2d2
cybermavka <svettacifra@gmail.com>
2022-10-12 16:54:27 +0300

    final.

--- src/main/java/lotr/CharacterFactory.java
+++ src/main/java/lotr/CharacterFactory.java
@@ -10,13 +10,18 @@ import static org.reflections.scanners.Scanners.SubTypes;
 
 public class CharacterFactory {
     @SneakyThrows
-    Character createCharacter(){
-        Reflections reflections = new Reflections();
+    static Character createCharacter(){
+        Reflections reflections = new Reflections("lotr");
         Set<Class<?>> subTypes =
                 reflections.get(SubTypes.of(Character.class).asClass());
         subTypes.remove(Noble.class);
         int choice = new Random().nextInt(subTypes.size());
         return (Character) subTypes.toArray(new Class[0])[choice].getDeclaredConstructor().newInstance();
 
+    }
+
+    public static void main(String[] args) {
+        lotr.Character ch = CharacterFactory.createCharacter();
+        System.out.println(ch);
     }
 }
